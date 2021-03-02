029d7e88d5da109593eb4a05de1156af43ce7e56
royalfelep <73192682+royalfelep@users.noreply.github.com>
2021-03-01 23:56:55 -0300

    Código refatorado

--- projetinhos/fizzbuzz.js
+++ projetinhos/fizzbuzz.js
@@ -1,12 +1,14 @@
 let numero = prompt('Escolha um número:')
 
 function fizzBuzz(numero) {
+    // Loop de 1 ao número escolhido
     for(let i = 1; i<=numero; i++){
+        // Verificações
         if(i % 3 == 0 && i % 5 == 0) console.log('FizzBuzz')
         else if(i % 3 == 0) console.log('Fizz')
         else if(i % 5 == 0) console.log('Buzz')
         else console.log(i);
     }
 }
-  
+
 fizzBuzz(numero);
