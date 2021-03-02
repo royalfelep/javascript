c33a2d35d4107f51daacf2f6591fbeb544049c54
royalfelep <73192682+royalfelep@users.noreply.github.com>
2021-03-01 21:53:16 -0300

    Melhorando o código

--- projetinhos/ascii-phrase-to-string.js
+++ projetinhos/ascii-phrase-to-string.js
@@ -1,32 +1,39 @@
 // Teste Nós Codamos - NUBANK
 
+// EncodedString recebe como parâmetro frase formada por números da tabela ascii invertidos
+encodedString = prompt('Qual código invertido ascii deseja converter?'); // recebe parâmetro
+decode(encodedString); // inicializa a função
+
 function decode(encodedString) {
-    //reverte a string recebida
-    encodedString = encodedString.split("").reverse().join("");
+
+    encodedString = encodedString.split("").reverse().join(""); //desinverte a string recebida
+
+    if(encodedString[0] === '0') return alert('Retire o 0 do início da string.'); // verifica se string começa com o número 0.
+
+    let str = '';   // Inicialização da string que nos dará o resultado
     
-    //loop com final igual ao tamanho da string;
-    for(let a = 0; a < encodedString.length ; a){
+    for(let a = 0; a < encodedString.length ; a){   //loop com final igual ao tamanho da string;
         
         // Concatenação dos elementos para validação em blocos de 2 e 3;
-       var three = encodedString[a] + encodedString[a+1] + encodedString[a+2];
-       var two = encodedString[a] + encodedString[a+1];
-       // Inicialização da string que nos dará o resultado
-       var str ="";
+       let three = encodedString[a] + encodedString[a+1] + encodedString[a+2];
+       let two = encodedString[a] + encodedString[a+1];
        
        // Validação 1: Se o bloco iniciar com o número '1', quer dizer que terá um valor de 3 dígitos na tabela ASCII.
        if(encodedString[a] === "1"){ 
             str += String.fromCharCode(three);
-            //Incremento de 3 digitos para mover ao próximo bloco.
-            a+=3;
+            a+=3; //Incremento de 3 digitos para mover ao próximo bloco.
        }
        
        // Validação 2: Se o bloco iniciar com valores diferentes de '1', quer dizer que terá um valor de 2 dígitos na tabela ASCII.
        else if(encodedString[a] !== "1"){
            str += String.fromCharCode(two);
-           a+=2;
+           a+=2; //Incremento de 2 digitos para mover ao próximo bloco.
        }  
     }
- return (str);
+ return console.log(str);
 }
 
-console.log
+/*
+ 33795010012390111166 | "Bom dia!" invertido
+ 101211501801101070 | "Felipe" invertido, porém com primeiro termo igual a 0 - teste de validação
+ */
