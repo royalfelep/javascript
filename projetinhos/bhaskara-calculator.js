0c2e9944641da0f29080ee10d1b10bf21ebf2dfa
royalfelep <73192682+royalfelep@users.noreply.github.com>
2021-03-02 00:43:46 -0300

    Código Refatorado

--- projetinhos/bhaskara-calculator.js
+++ projetinhos/bhaskara-calculator.js
@@ -10,7 +10,7 @@ function bhaskara(variavel){
 let delta = Math.pow(variavel.b,2) - (4 * (variavel.a * variavel.c)) // Cálculo do Delta (b^2-4*a*c)
 let x1 = ((-variavel.b) + Math.sqrt(delta,2)) / (2 * variavel.a) // x1 = (-b + sqrt(delta)) / 2*a
 let x2 = ((-variavel.b) - Math.sqrt(delta,2)) / (2 * variavel.a) // x2 = (-b - sqrt(delta)) / 2*a
-return console.log(`x1 = ${x1} e x2 = ${x2}`)
+return alert(`x1 = ${x1} \n x2 = ${x2}`)
 }
 
 bhaskara(variavel) // parâmetros
